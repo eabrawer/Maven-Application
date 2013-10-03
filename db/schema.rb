d24dab95389da1e0b7f0a891cfc48dad3caddf3d
eabrawer <ericbrawer@gmail.com>
2013-10-02 23:59:59 -0400

    Creating project model and basic validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131002022738) do
+ActiveRecord::Schema.define(version: 20131003034936) do
+
+  create_table "projects", force: true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.text     "essay"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name"
