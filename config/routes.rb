4e72b0314d8219649a6a4370ce5a9ee02f0dd106
eabrawer <ericbrawer@gmail.com>
2013-10-02 23:33:20 -0400

    Creating a sessions controller with log in functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Letsmeet::Application.routes.draw do
 
+  # get "sessions/new"
+  # get "sessions/create"
+  # get "sessions/destroy"
+  resources :sessions, :only => [:new, :create, :destroy]
   resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
