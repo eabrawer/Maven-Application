f40d72d637e87dcaebca75b0230d7a3ab8add5a7
eabrawer <ericbrawer@gmail.com>
2013-10-02 21:15:27 -0400

    Creating flash notices and alerts in the user controller and are shown via the application.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,10 @@ class UsersController < ApplicationController
   def update
   	@user = User.find(params[:id])
   	if @user.update_attributes(user_params)
-  		redirect_to user_path
+  		redirect_to user_path :notice => "User was successfully updated."
   	else
-  		render "edit", :alert => "you have an error"
+      flash.now[:alert] = "User was not updated"
+  		render "edit"
   	end
   end
 
@@ -27,8 +28,9 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-  		redirect_to users_url
+  		redirect_to @user, :notice => "User was successfully created."
   	else
+      flash.now[:alert] = "User was not created"
   		render "new"
   	end
   end
