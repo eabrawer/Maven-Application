11b041b7fc67893517d61703e2c07f6f1b203572
Szuturon <ronnyszutu@gmail.com>
2013-10-07 17:25:45 -0400

    More fixes for Rails 3 downgrade

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,12 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Letsmeet::Application.config.secret_key_base = '88ff0b3916357ff35f1e5ad52b83cb16b3869f0fc70018551d303f2dee4264f2f6f90910bc97604df83b542bdf1c825571b534f9f3575719daa2a6f1c70c32b8'
+#Letsmeet::Application.config.secret_key_base = '88ff0b3916357ff35f1e5ad52b83cb16b3869f0fc70018551d303f2dee4264f2f6f90910bc97604df83b542bdf1c825571b534f9f3575719daa2a6f1c70c32b8'
+
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Letsmeet::Application.config.secret_token = '13647be97436550421d8b1e30785cdf4d9d6bc253e8d91ff199441a2fc96028acb19f7887ea2efa9a672112690e97e6c1319413093650bb1247da64396f765ab1'
