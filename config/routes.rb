951acf4f3e78e06a58ced0f4f7e80c94af219c7b
Szuturon <ronnyszutu@gmail.com>
2013-10-13 16:18:20 -0400

    Set up routes for my::projects

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,13 @@ Letsmeet::Application.routes.draw do
 
   resources :projects do
   	member { post :vote }
-	resources :comments, :only => [:create, :destroy, :new] do
-		member { post :vote }
-	end
+		resources :comments, :only => [:create, :destroy, :new] do
+			member { post :vote }
+		end
+  end
+
+  namespace :my do
+  	resources :projects, :only => [:index]
   end
 
 
