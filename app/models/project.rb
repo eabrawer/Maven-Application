2e84735a639766560f68de8327c705367c1fe8a9
Szuturon <ronnyszutu@gmail.com>
2013-10-13 18:03:44 -0400

    Write tag helper methods in project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -13,4 +13,23 @@ class Project < ActiveRecord::Base
 	has_many :comments
 	has_many :taggings
 	has_many :tags, through: :taggings
+
+	def self.tagged_with(name)
+  	Tag.find_by_name!(name).projects
+	end
+
+	def self.tag_counts
+	  Tag.select("tags.*, count(taggings.tag_id) as count").
+	    joins(:taggings).group("taggings.tag_id")
+	end
+
+	def tag_list
+	  tags.map(&:name).join(", ")
+	end
+
+	def tag_list=(names)
+	  self.tags = names.split(",").map do |n|
+	    Tag.where(name: n.strip).first_or_create!
+	  end
+	end
 end
